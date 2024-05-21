52ca9ce541fcb5544be1636a279a6a1933160a3a
js1251 <sailer.jakob@web.de>
2024-05-21 22:09:42 +0200

    converted shared projects to NET standard 2.0

--- shared_bspFormat/Lumps/OriginalFacesBspLump.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace shared_bspFormat.Lumps;
-
-public sealed class OriginalFacesBspLump : FacesBspLump {
-    public const int ID = 27;
-    public OriginalFacesBspLump(byte[] bytes) : base(bytes) { }
-}
